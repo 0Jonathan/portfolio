64b8059580c4f5444cd5515a2b9be69ac2e90da8
0Jonathan <jonathan.frachet@outlook.fr>
2024-01-16 10:29:46 +0100

    portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+import { useTranslation } from 'react-i18next';
+import i18n from '../utils/i18n';
+
+const Navbar = () => {
+
+  const { t } = useTranslation();
+
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+  const [isEnglish, setIsEnglish] = useState(true); // true for English, false for Spanish
+
+  const toggleLanguage = () => {
+    const newLang = isEnglish ? 'fr' : 'en';
+    setIsEnglish(!isEnglish);
+    i18n.changeLanguage(newLang);
+  };
+  
+  return (
+    <nav
+    className={`
+    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+       <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+        <Link 
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+        <img src={logo} alt= "logo" className="w-9 h-9 object-contain" />
+        <p className="text-white text-[18px] font-bold cursor-pointer flex">Jonathan&nbsp;<span className="sm:block hidden">| {t('job')}
+        </span>
+        </p>
+        </Link>
+
+            {/* Language Toggle Switch */}
+        <div className="language-toggle">
+          <input 
+            id="lang-toggle" 
+            type="checkbox" 
+            checked={isEnglish} 
+            onChange={toggleLanguage} 
+            className="checkbox" 
+          />
+          <label htmlFor="lang-toggle" className="label">
+            <span className="inner-circle"></span>
+            <span className="switch-text">EN | FR</span>
+          </label>
+        </div>
+
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => 
+          <li 
+          key={link.id}
+          className={`${
+            active === link.title
+            ? "text-white"
+            : "text-secondary"
+          } hover:text-white text-[18px] font-medium cursor-pointer`}
+          onClick={() => setActive(link.title)}
+        >
+          <a href={`#${link.id}`}>{link.title}</a>
+          </li>)}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}/>
+
+          <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+          <ul className="list-none flex sm:flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => 
+          <li 
+          key={link.id}
+          className={`${
+            active === link.title
+            ? "text-white"
+            : "text-secondary"
+          } font-poppins font-medium cursor-pointer text-[16px]`}
+          onClick={() => { setToggle(!toggle); setActive(link.title);
+          }}
+        >
+          <a href={`#${link.id}`}>{link.title}</a>
+          </li>)}
+        </ul>
+          </div>
+        </div>
+       </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/utils/i18n.js
@@ -0,0 +1,82 @@
+// i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          // -------------------------NAVBAR--------------------------
+          "job": "Professional Web Developper",
+          // -------------------------ABOUT---------------------
+          "mywork": "My work",
+          "myproject": "Projects.",
+          "projecttext": "Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.",
+          // ------------------------WORK-------------------------
+          "overview": "Overview.",
+          "overviewtext": "I'm a skilled software developer with experience in Javascript, HTML, CSS and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!",
+          // -----------------------CONTACT-------------------------
+          "getintouch": "Get in touch",
+          "yourname": "Your Name",
+          "yourname_ph": "What's your name?",
+          "youremail": "Your Email",
+          "youremail_ph": "What's your email?",
+          "yourmessage": "Your message",
+          "yourmessage_ph": "What do you want to say?",
+          "contact_sent": "Thank you, I will get back to you as soon as possible.",
+          // --------------------HERO--------------------
+          "hi_i_am": "Hi, I am",
+          "what_i_do": "I develop 3D visuals, user interfaces, ",
+          "what_i_do_next": "application web,  site d'E-commerce ",
+          // --------------------EXPERIENCE------------------
+          "experience_subtext": "What I have done so far",
+          "experience_headtext": "Work Experience.",
+          // -------------------FEEDBACKS--------------------
+          "feedbacks_subtext": "What others say",
+          "feedbacks_headtext": "Testimonials."
+          // Add other translation keys and values here
+        }
+      },
+      fr: {
+        translation: {
+          // -------------------------NAVBAR--------------------------
+          "job": "Développeur Web Professionnel",
+          // -------------------------ABOUT---------------------
+          "mywork": "Mon travail",
+          "myproject": "Projets.",
+          "projecttext": "Les projets suivants mettent en valeur mes compétences et mon expérience à travers des exemples concrets de mon travail. Chaque projet est brièvement décrit avec des liens vers des référentiels de code et des démos en direct. Cela reflète ma capacité à résoudre des problèmes complexes, à travailler avec différentes technologies et à gérer des projets effectivement.",
+          // ------------------------WORK-------------------------
+          "overview": "Aperçu.",
+          "overviewtext": "Je suis un développeur de logiciels qualifié avec une expérience en Javascript, HTML, CSS et une expertise dans des frameworks comme React, Node.js et Three.js. J'apprends vite et je collabore étroitement avec les clients pour créer des solutions efficaces, évolutives et conviviales qui résolvent des problèmes du monde réel. Travaillons ensemble pour donner vie à vos idées !",
+          "getintouch": "Entrons en contact",
+          "yourname": "Votre Nom",
+          "yourname_ph": "Quel est votre nom?",
+          "youremail": "Votre Email",
+          "youremail_ph": "Quel est votre email?",
+          "yourmessage": "Votre Message",
+          "yourmessage_ph": "Que voulez-vous dire?",
+          // --------------------HERO--------------------
+          "hi_i_am": "Je suis",
+          "what_i_do": "Je développe des visuels 3D , des interfaces utilisateurs, ",
+          "what_i_do_next": "application web,  site d'E-commerce ",
+          // --------------------EXPERIENCE------------------
+          "experience_subtext": "Ce que j'ai fais jusqu'à présent",
+          "experience_headtext": "Expérience professionnelle.",
+          // -------------------FEEDBACKS--------------------
+          "feedbacks_subtext": "Ce que les gens disent",
+          "feedbacks_headtext": "Témoignages."
+
+          // Corresponding keys in French
+        }
+      }
+    },
+    lng: "en", // initial language
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
